b50494a5b75fef28cd8ce9a2702258adf3576caa
takahashikosuke <takahashikosuke@team-lab.com>
2022-12-04 20:06:33 +0900

    :wrench: call api

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
     <h1>This is Sample page</h1>
+    <p>{{ displayData }}</p>
   </div>
 </template>
+
+<script setup lang="ts">
+const { $axios } = useNuxtApp();
+
+const displayData = ref<any>(undefined);
+
+useAsyncData(async () => {
+  // console.log("called");
+
+  try {
+    const data = await $axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
+    displayData.value = JSON.stringify(data.data.id);
+    // console.log(data);
+  } catch (error) {
+    console.error("error!");
+  }
+});
+</script>
