1414f03f4a240a9965d4d7a6a1b18a5f18e4a4ac
takahashikosuke <takahashikosuke@team-lab.com>
2022-12-11 18:46:45 +0900

    :wrench: store sample

---
+++ src/store/test.ts
@@ -0,0 +1,17 @@
+// Refはvue本体からとってこれそう
+import type { Ref } from "vue";
+
+export const useCounter = () => {
+  // stateの定義
+  const counter: Ref<number> = useState("counter", () => 0);
+
+  // stateの更新処理
+  const updateCounter = (counter: Ref<number>) => (value: number) => {
+    counter.value = value;
+  };
+
+  return {
+    counter: readonly(counter), // stateはreadonlyとし、update関数を通してのみ更新できる
+    updateCounter: updateCounter(counter),
+  };
+};
