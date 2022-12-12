753d587ae3a512c232fc9bd7fa82b09be612d627
takahashikosuke <takahashikosuke@team-lab.com>
2022-12-12 21:04:57 +0900

    :wrench: そのまま入れても大丈夫かテスト

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,7 @@
     <h1>This is Sample page</h1>
     <p>{{ displayData }}</p>
     <p>{{ `id is : ${id}` }}</p>
+    <p>{{ test }}</p>
   </div>
 </template>
 
@@ -13,6 +14,8 @@ const { $axios } = useNuxtApp();
 
 const { id } = useId();
 
+const test = ref<string>();
+
 const displayData = ref<any>(undefined);
 
 useAsyncData(async () => {
@@ -21,7 +24,9 @@ useAsyncData(async () => {
   try {
     const data = await $axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
     displayData.value = JSON.stringify(data.data.id);
-    // console.log(data);
+
+    const wa = "test value is shown";
+    test.value = wa;
   } catch (error) {
     console.error("error!");
   }
