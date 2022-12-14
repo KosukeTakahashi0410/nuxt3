a29feaa344bbc0687c647c86ba5d7500e0def8e9
takahashikosuke <takahashikosuke@team-lab.com>
2022-12-14 21:31:28 +0900

    :wrench: テストコンポーネントを一旦追加

---
+++ src/components/testCompo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>This is Sample page</h1>
+    <p>{{ displayData }}</p>
+    <p>{{ `id is : ${id}` }}</p>
+    <p>{{ test }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useId } from "~/store/test2";
+const { $axios } = useNuxtApp();
+
+const { id } = useId();
+
+const test = ref<string>();
+
+const displayData = ref<any>(undefined);
+
+useAsyncData(async () => {
+  try {
+    const data = await $axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
+    displayData.value = JSON.stringify(data.data.id);
+  } catch (error) {
+    console.error("error!");
+  }
+});
+</script>
