aacc687a38d8f1a3ec35cdb4b7b4cac7cee616b8
takahashikosuke <takahashikosuke@team-lab.com>
2022-12-22 22:22:29 +0900

    :wrench: コメントを追加

--- src/pages/sample.vue
+++ src/pages/sample.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <!-- hiddenなinputにcomputedの値を当てる -->
+    <!-- xxx.propertyで取れる -->
     <input type="hidden" id="warota" :value="hiddenName.name" />
     <input type="hidden" id="kusowarota" :value="hiddenName.en_name" />
     <p>{{ `name: ${hiddenName.name}` }}</p>
@@ -22,32 +24,40 @@
 </template>
 
 <script setup lang="ts">
+// 日本語と英語でマップされた値
 const OPTIONS = [
   { value: 1, name: "わろた", en_name: "warota" },
   { value: 2, name: "草", en_name: "kusa" },
   { value: 3, name: "真顔", en_name: "magao" },
 ];
 
+// 選択肢を作る（value, labelでマップ）
 const INPUT_OPTIONS = OPTIONS.map(({ value, name }) => {
   return { value, label: name };
 });
 
+// 選択した値（state）
 const state = reactive<{ value: number }>({
   value: INPUT_OPTIONS[0].value,
 });
 
+// 変更のハンドラ
 const onChange = (event: any) => {
   state.value = parseInt(event.target.value);
 };
 
+// computedでhiddenに突っ込む値をとる
 const hiddenName = computed(() => {
+  // 選択した値を持ってOPTIONSから当てはまるものを取得
   const selectedOption = OPTIONS.find(({ value }) => {
     return value === state.value;
   });
 
+  // 選択した値を返す
   return selectedOption ?? OPTIONS[0];
 });
 
+// クリック時の処理、今は無視する
 const onClick = () => {
   const warota: HTMLInputElement = <HTMLInputElement>(
     document.getElementById("warota")
